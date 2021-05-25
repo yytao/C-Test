2e6999eaa0acbaba5744ea738f1eb5f67f6a9e84
Taoyy <tyyphp@163.com>
2021-05-25 14:47:25 +0800

    debug

--- cpp/1_floatANDdouble.cpp
+++ cpp/1_floatANDdouble.cpp
@@ -1,19 +1,20 @@
 #include  <iostream>   
 using namespace std;   
 int test(int a,int b);   
-float test(float a,float b);   
+float testf(float a,float b);   
 int main()
 {   
-    cout<<test(1,2) << endl;
-    cout<<test(2.1, 3.14)  << endl;
-    cin.get();   
+    cout << test(1,2) << endl;
+    cout << testf(2.1, 3.14)  << endl;
+    cin.get();
 }   
+
 int test(int a,int b)   
 {   
     return a+b;   
 }   
    
-float test(float a,float b)   
+float testf(float a,float b)   
 {   
     return a+b;   
 }
