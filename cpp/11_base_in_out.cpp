0280dc75932258b8ee08ad92a094ef8833bdc00b
Taoyy <tyyphp@163.com>
2021-05-25 16:58:31 +0800

    add new file

---
+++ cpp/11_base_in_out.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    char str1[] = "Hello C++";
+    cout << str1 << endl;
+
+    char name[50];
+    cin >> name;
+    cout << "你的名字是：" << name << endl;
+
+
+}
