cae7899c86af2274d9e1acee5a4c5e842af92a5b
Taoyy <tyyphp@163.com>
2021-06-03 16:17:31 +0800

    add new file

--- cpp/object/3_class_copy.cpp
+++ cpp/object/3_class_copy.cpp
@@ -26,11 +26,58 @@ class CExample
     
 };
 
+class Line
+{
+    public:
+        int getLength();
+        Line( int len);
+        Line( const Line &obj);
+        ~Line();
+
+    private:
+        int *ptr;
+
+};
+
+Line::Line(int len)
+{
+    cout << "Normal constructor allocating ptr" << endl;
+    ptr = new int;
+    *ptr = len;
+}
+
+Line::Line(const Line &obj)
+{
+    cout << "Copy constructor allocating ptr" << endl;
+    ptr = new int;
+    *ptr = *obj.ptr;
+}
+
+Line::~Line()
+{
+    cout << "Freeing memory" << endl;
+    delete ptr;
+}
+
+int Line::getLength()
+{
+    return *ptr;
+}
+
+void display(Line obj)
+{
+    cout << obj.getLength() << endl;
+}
+
 int main()
 {
-    CExample A(100);
-    CExample B = A;
-    B.show();
+    // CExample A(100);
+    // CExample B = A;
+    // B.show();
     
+    Line line(10);
+
+    display(line);
+
     return 0;
 }
