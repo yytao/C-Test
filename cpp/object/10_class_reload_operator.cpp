60a4e8d908b16cc5fc096d9ccc1f06c2e5a0b27e
Taoyy <tyyphp@163.com>
2021-07-06 14:11:42 +0800

    add new learn

---
+++ cpp/object/10_class_reload_operator.cpp
@@ -0,0 +1,52 @@
+/**
+ * 重载运算符
+ * 
+ */ 
+
+#include <iostream>
+using namespace std;
+
+class Box
+{
+    public:
+        double getVolume(void)
+        {
+            return width * height * breadth;
+        }
+        Box(double h=0, double w=0, double b=0):height(h),width(w),breadth(b){};
+
+        Box operator+(const Box &b)
+        {
+            Box box;
+            box.height = this->height + b.height;
+            box.width = this->width + b.width;
+            box.breadth = this->breadth + b.breadth;
+            cout << "calling" << endl;
+            return box;
+        }
+    protected:
+        double height;
+        double width;
+        double breadth;
+
+};
+
+int main()
+{
+
+    Box A(1,2,3);
+    Box B(1,2,3);
+
+    Box C;
+    C = A + B;
+
+    double volumeA = A.getVolume();
+    cout << volumeA << endl;
+
+    double volumeB = B.getVolume();
+    cout << volumeB << endl;
+
+    double volumeC = C.getVolume();
+    cout << volumeC << endl;
+    return 0;
+}
