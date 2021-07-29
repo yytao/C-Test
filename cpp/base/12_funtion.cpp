680c629d0508414d71180170a03880e848818e75
Taoyy <tyyphp@163.com>
2021-07-29 17:22:47 +0800

    mod

--- cpp/base/12_funtion.cpp
+++ cpp/base/12_funtion.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 //函数
 
+//前置声明，具体实现可以在文件内任意地方，形参列表可省略变量名，只写数据类型
+int sum(int, int);
+
+
 int main()
 {
+    
+    int sumNumber;
+    sumNumber = sum(3, 4);
+    
+    cout << sumNumber << endl;
+}
 
+int sum(int a, int b)
+{
+    return a+b;
 }
